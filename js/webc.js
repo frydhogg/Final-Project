0233b67ccf5bd8651a67334923607d24d75d63d3
xsheizen <48442932+xsheizen@users.noreply.github.com>
2019-03-23 22:39:50 +0700

    add html, folder js dan css

---
+++ js/webc.js
@@ -0,0 +1,19 @@
+var typed = new Typed('#sub-content-3', {
+  strings: ['Basic Javascript'],
+  typeSpeed: 70,
+  loop : true,
+  loopCount : 5
+});
+
+var typed = new Typed('#sub-content-4', {
+  strings: ['Advance Javascript', 'Function, Array, Object'],
+  typeSpeed: 70,
+  loop : true,
+  loopCount : 2
+});
+
+var typed = new Typed('#text-about', {
+  strings: ['Frydho Ghazian Albajili', 'Huge Interest to Become Full Stack Developer', 'Nearest Interest to Become Front End Developer', 'Powered by Coffee and Dreams'],
+  typeSpeed: 70,
+  loop : true,
+});
